1a66a932893205569bc00b35cab99d272da96009
angelsusum <angelsusum@gmail.com>
2024-05-04 02:41:53 +0900

    Feat: 플레이어 무기 조작 구현

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] // unity에서 값 변경 가능
-    private float moveSpeed;
-    Animator anim;
-    // Start is called before the first frame update
-    void Start()
+    private float moveSpeed; // 플레이어 이동 속도
+    [SerializeField]
+    private InputActionReference attack, pointerPosition; // input action
+    private Vector2 pointerInput; // 마우스포인터 위치
+    private WeaponParent weaponParent;
+    
+    Animator anim; // 플레이어 움직임 애니메이션
+ 
+    void Awake()
     {
-        anim = GetComponent<Animator>();   
+        anim = GetComponent<Animator>();
+        weaponParent = GetComponentInChildren<WeaponParent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        pointerInput = GetPointerInput();
+        weaponParent.PointerPosition = pointerInput;
         float inputX = Input.GetAxisRaw("Horizontal"); // 수평이동
         float inputY = Input.GetAxisRaw("Vertical"); // 수직이동
 
@@ -29,5 +37,15 @@ public class Player : MonoBehaviour
 
         Vector3 moveTo = new Vector3(inputX, inputY, 0);
         transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+        if (attack.action.triggered)
+            weaponParent.Attack(pointerInput);
+    }
+
+    private Vector2 GetPointerInput()
+    {
+        Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
