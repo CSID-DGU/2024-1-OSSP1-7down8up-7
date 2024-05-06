f68ec83bb874ee2ce3f884a65b3972bf6bea3939
angelsusum <angelsusum@gmail.com>
2024-05-06 16:42:35 +0900

    Feat: 카메라 무빙

---
+++ Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    Vector3 displacement;
+
+    public Vector2 center;
+    public Vector2 size;
+    float height;
+    float width;
+
+    void Start()
+    {
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
+        displacement = transform.position - target.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, size);
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+
+            float lx = size.x * 0.5f - width;
+            float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
+
+            float ly = size.y * 0.5f - height;
+            float clampY = Mathf.Clamp(transform.position.y, -ly + center.y, ly + center.y);
+
+            transform.position = new Vector3(clampX, clampY, -10f);
+        }
+    }
+}
