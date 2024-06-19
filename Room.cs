541fd5d27872cf160ef2de73d19e46a7a4c084a9
Chaeyeon26 <129031536+Chaeyeon26@users.noreply.github.com>
2024-06-19 17:27:06 +0900

    feat: 방 타입

---
+++ Room.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room {
+	public Vector2 gridPos;
+	public int type;
+	public bool doorTop, doorBot, doorLeft, doorRight;
+	public int nextToBossRoom;
+	public Room(Vector2 _gridPos, int _type){
+		gridPos = _gridPos;
+		type = _type;
+	}
+}
